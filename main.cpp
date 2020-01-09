7cf175d6155db304d61e18f4e698f5765624fd20
Luis Leon <lleon95@gmail.com>
2020-01-09 18:18:26 +0100

    Add tests for insertion and search

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main(){
   bst<int, int> mytree;
   auto pair = std::make_pair(50,3);
 
-  auto result = mytree.insert(pair);
+  auto result = mytree.insert(std::make_pair(50,3));
   std::cout << std::get<1>(result) << std::endl;
 
   for(int i = 0; i < 10; ++i) {
@@ -20,12 +20,25 @@ int main(){
   }
 
   /* Print the tree */
-  auto begin = mytree.begin();
-  auto end = mytree.end();
-  std::cout << "Next: " << std::get<0>(*begin) << std::endl;
-
   for (auto i : mytree) {
     std::cout << "Node: " << std::get<0>(i) << std::endl;
   }
+
+  /* Look for a key */
+  auto elem = mytree.find(16);
+  auto end = mytree.end();
+  if (elem == end) {
+    std::cout << "Holder: empty" << std::endl;
+  } else {
+    std::cout << "Holder: " << std::get<0>(*elem) << "," << std::get<1>(*elem) << std::endl;
+  }
+
+  /* Clear */
+  mytree.clear();
+  /* Print the tree */
+  for (auto i : mytree) {
+    std::cout << "Node: " << std::get<0>(i) << std::endl;
+  }
+  
   return 0;
 }
